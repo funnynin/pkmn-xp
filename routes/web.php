d7275f10da5c0f4d02440de302a417d82f92689c
Funnynin <5513830+funnynin@users.noreply.github.com>
2025-05-06 14:20:10 +0100

    basic response tests

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // gets converted to HTML - does not return plaintext!
+    return 'hello world';
+});
+
+Route::get('/pokemon', function () {
+    // this is smartly converted to json // raw response
+    return [1, 2, 3];
 });
